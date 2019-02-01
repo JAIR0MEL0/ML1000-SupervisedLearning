a48f90fbf0819ffc4f658c7a5ab4fb6793da9a9d
JAIR0MEL0 <46827696+JAIR0MEL0@users.noreply.github.com>
2019-02-01 15:28:36 -0500

    Update Retail_Group_Ass1_Kickstarer.R

--- Retail_Group_Ass1_Kickstarer.R
+++ Retail_Group_Ass1_Kickstarer.R
@@ -1,9 +1,10 @@
 ###############################################################################################
 ####################    Kickstarter lookalike and/or propensity model    #####################
 ###############################################################################################
-#import packages;
+
+library(lattice)
+library(ggplot2)
 library(caret)
-library(pROC)
 
 #################################################################################
 ##########################    IMPORING DATASET     ##############################
@@ -20,31 +21,27 @@ head(data,10)
 str(data)
 summary(data)
 
+#################################################
+# Data Preparation
+#################################################
 
-#Add a new column to make a clear Binary classification
+
 data[["successful"]] <- ifelse(data$state=="successful",'YES','NO')
 
-#Create a factor for the new column
 data$successful <- factor(data$successful, labels = c('NO', 'YES'))
 
-#We select only the fields with more relevant significant . We'll use USD Pledged as it's a normalized currency for all rojects
-data <- data[c('name','category','main_category','currency','deadline','goal','usd_goal_real','state','backers','country','successful','usd.pledged','usd_pledged_real')]
+data <- data[c('category','main_category','currency','backers','country','deadline','name','goal','usd_goal_real','successful','usd.pledged','usd_pledged_real')]
+
+# dummy variables for factors/characters
+data$category <- as.factor(data$category)
+data$main_category <- as.factor(data$main_category)
+data$currency <- as.factor(data$currency)
+data$backers <- as.factor(data$backers)
+data$country <- as.factor(data$country)
 
 # what is the proportion of our outcome variable?
-prop.table(table(data$successful))
-
-
-# save the outcome for the glmnet model
-tempOutcome <- data$successful
-
-# generalize outcome and predictor variables
-outcomeName <- 'successful'
-predictorsNames <- names(data)[names(data) != outcomeName]
-
-
-#################################################
-# model it
-#################################################
+percentage <- prop.table(table(data$successful)) * 100
+cbind(freq=table(data$successful), percentage)
 
 # pick model gbm and find out what type of model it is
 getModelInfo()$gbm$type
@@ -52,43 +49,103 @@ getModelInfo()$gbm$type
 # split data into training and testing chunks
 set.seed(1234)
 
-#This is to use the same data set for Training and Test; 2018 has sufficient records; but we can use different year for Training and Testing
+#This is to use the same data set for Training and Test
 splitIndex <- createDataPartition(data[,outcomeName], p = .75, list = FALSE, times = 1)
 trainDF <- data[ splitIndex,]
 testDF  <- data[-splitIndex,]
 
-objControl <- trainControl(method = 'cv', number=3, returnResamp='none')
+#Dimensions of the dataset
+dim(trainDF)
 
-# run model
 
-#Still working on the model... the predictors and the Successful factors.  It's showing an error.  Need to sitdown and look it deeper
-objModel <- train(trainDF[,predictorsNames], as.factor(trainDF[,outcomeName]), 
-                  method='gbm', 
-                  trControl=objControl,  
-                  metric = "ROC",
-                  preProc = c('knnImpute', 'pca'))
+#list types for each attribute
+sapply(trainDF, class)
 
-# find out variable importance
-summary(objModel)
+#There are only two level so it's a Binary classification problem.  
+#If we had selected state, there are multiple levels so it would be a multinomial classifgication.
+#I this case, we'll used the converted succesful as we only need to know if the project successed or not
+levels(trainDF$successful)
 
-# find out model details
-objModel
+#split input and output
+x <- trainDF[,1:5]
+y <- trainDF[,10]
 
+#################################################
+# Visualization
+#################################################
+
+
+
+#boxplot for each attribute on one image
+par(mfrow=c(1,5))
+  for(i in 1:5){
+    boxplot(x[,i], main=names(trainDF)[i]) #getting an error in here... I think this is related to the vector x
+}
+
+
+#barplot for class breakdown
+plot(y)
+
+#I need to understand why the vector is not recognized.
+
+
+
+featurePlot(x=x, y=y, plot = "ellipse")
+
+#Multivariate plots
+featurePlot(x=x, y=y, plot ="box")
+
+#density plots for each attribute by class value
+scales <- list(x=list(relation="free"), y=list(relation="free"))
+featurePlot(x=x, y=y, plot="density", scales=scales)
+
+
+
+#Let's evaluate some alhorithms
+control <- trainControl(method = "cv", number = 10)
+metric <- "Accuracy"
+
+#################################################
+# model it
+#################################################
+
+
+#Now we are building some models
+# a) linear algorithms
+set.seed(7)
+fit.lda <- train(successful~., data=trainDF, method="lda", metric=metric, trControl=control)
+# b) nonlinear algorithms
+# CART
+set.seed(7)
+fit.cart <- train(successful~., data=trainDF, method="rpart", metric=metric, trControl=control)
+# kNN
+set.seed(7)
+fit.knn <- train(successful~., data=trainDF, method="knn", metric=metric, trControl=control)
+# c) advanced algorithms
+# SVM
+set.seed(7)
+fit.svm <- train(successful~., data=trainDF, method="svmRadial", metric=metric, trControl=control)
+# Random Forest
+set.seed(7)
+fit.rf <- train(successful~., data=trainDF, method="rf", metric=metric, trControl=control)
 
 #################################################
 # evalutate model
 #################################################
-# get predictions on our testing data
+#Now let's select the best model
+#summarize accuracy of models
+results <- resamples(list(lda=fit.lda, cart=fit.cart, knn=fit.knn, svm=fit.svm, rf=fit.rf))
+summary(results)
 
-# class prediction
-predictions <- predict(object=objModel, testDF[,predictorsNames], type='raw')
-head(predictions)
-postResample(pred=predictions, obs=as.factor(testDF[,outcomeName]))
+#Visualize the accuracy of the models
+dotplot(results)
 
 
-# probabilities 
-predictions <- predict(object=objModel, testDF[,predictorsNames], type='prob')
-head(predictions)
-postResample(pred=predictions[[2]], obs=ifelse(testDF[,outcomeName]=='YES',1,0))
-auc <- roc(ifelse(testDF[,outcomeName]=="YES",1,0), predictions[[2]])
-print(auc$auc)
+#summarize best model
+print(fit.lda)
+
+
+#Let's make a prediction
+#Estimate skill of LDA on trainDF dataset
+predictions <- predict(fit.lda, trainDF)
+confusionMatrix(predictions,trainDF$successful)
