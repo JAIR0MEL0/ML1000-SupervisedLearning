307fed81dafc26e5ed09614e8c9e11e0cb0d7e2f
JAIR0MEL0 <46827696+JAIR0MEL0@users.noreply.github.com>
2019-02-07 13:25:04 -0500

    Update Retail_Group_Ass1_Kickstarer.R

--- Retail_Group_Ass1_Kickstarer.R
+++ Retail_Group_Ass1_Kickstarer.R
@@ -89,7 +89,24 @@ plot_backers_by_main_category <- only_successful_data %>%
 
 plot_backers_by_main_category
 
-#We'll examine the time 
+#Let's review if the campaing of the project affects the success rate
+
+#Kickstarter allow only 60 days. They recommends that projects be set to 30 days or less. 
+#The idea is that if you project has not been funded within 30 days, it's not likely to be funded 
+#by their deadline either.
+
+length.pct <- data %>%
+  filter(state %in% c("successful", "failed"), campaing <= 61) %>%
+  group_by(campaing, state) %>%
+  summarise(count=n()) %>%
+  mutate(pct=count/sum(count))
+
+ggplot(length.pct[length.pct$state=="successful",], aes(campaing, pct)) + 
+  geom_point(colour="royalblue4", size=2.5) + ggtitle("Success vs. Campaing") + 
+  xlab("Project Campaing (Days)") + ylab("Success Rate (%)") + 
+  scale_x_continuous(breaks=c(0,10,20,30,40,50,60)) + geom_vline(xintercept=30, colour="red") + 
+  theme(plot.title=element_text(hjust=0.5), axis.title=element_text(size=12, face="bold"))
+
 
 ###############################################
 # Exploration of relation between amount of
